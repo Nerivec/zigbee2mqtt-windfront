74f10c0a54c91ef2389bf7304c8d72b6a23d4807
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-08-05 00:16:54 +0200

    fix: merge exposes filtering functions

--- src/components/dashboard-page/index.tsx
+++ src/components/dashboard-page/index.tsx
@@ -1,4 +1,4 @@
-import type { AnySubFeature, BasicFeature, FeatureAccessMode, FeatureWithAnySubFeatures, FeatureWithSubFeatures } from "../../types.js";
+import type { FeatureAccessMode } from "../../types.js";
 
 const BLACKLISTED_PARTIAL_FEATURE_NAMES = ["schedule_", "_mode", "_options", "_startup", "_type", "inching_", "cyclic_"];
 
@@ -18,7 +18,7 @@ const BLACKLISTED_FEATURE_NAMES = [
 
 const WHITELISTED_FEATURE_NAMES = ["state", "brightness", "color_temp", "mode", "sound", "occupancy", "tamper", "alarm", "action", "contact"];
 
-const isValid = (name: string | undefined, _access: FeatureAccessMode): boolean => {
+export const isValidForDashboard = (name: string | undefined, _access: FeatureAccessMode): boolean => {
     if (name) {
         if (WHITELISTED_FEATURE_NAMES.includes(name)) {
             return true;
@@ -36,28 +36,4 @@ const isValid = (name: string | undefined, _access: FeatureAccessMode): boolean 
     }
 
     return true;
-};
-
-export const getDashboardFeatures = (feature: BasicFeature | FeatureWithSubFeatures): FeatureWithAnySubFeatures | undefined => {
-    const { name, access } = feature;
-
-    if (!isValid(name, access)) {
-        return undefined;
-    }
-
-    if ("features" in feature && feature.features && feature.features.length > 0) {
-        const features: AnySubFeature[] = [];
-
-        for (const subFeature of feature.features) {
-            const validFeature = getDashboardFeatures(subFeature);
-
-            if (validFeature && !features.some((f) => f.property === validFeature.property)) {
-                features.push(validFeature);
-            }
-        }
-
-        return { ...feature, features };
-    }
-
-    return { ...feature };
 };
