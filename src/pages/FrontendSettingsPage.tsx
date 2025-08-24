46ceb0929fd8c8bd46b89ca68b0db90aaefc3c9a
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-08-24 13:31:56 +0200

    feat: add concentric3d layout

--- src/pages/FrontendSettingsPage.tsx
+++ src/pages/FrontendSettingsPage.tsx
@@ -225,6 +225,7 @@ export default function FrontendSettingsPage() {
                     <option value="radialOut2d">radialOut2d</option>
                     <option value="radialOut3d">radialOut3d</option>
                     <option value="concentric2d">concentric2d</option>
+                    <option value="concentric3d">concentric3d</option>
                 </SelectField>
                 <SelectField
                     name="network:label_type"
