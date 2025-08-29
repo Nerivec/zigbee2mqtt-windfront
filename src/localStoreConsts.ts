711fb937fcf04d3e9a86d55e340e5ef1b6e5abb4
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-08-29 14:28:01 +0200

    fix: cleanup network map config, remove from frontend settings page

--- src/localStoreConsts.ts
+++ src/localStoreConsts.ts
@@ -15,11 +15,7 @@ export const TABLE_COLUMNS_KEY = "table-columns";
 
 //-- Network
 export const NETWORK_RAW_DISPLAY_TYPE_KEY = "network-raw-display-type";
-export const NETWORK_MAP_LAYOUT_TYPE_KEY = "network-map-layout-type";
-export const NETWORK_MAP_LABEL_TYPE_KEY = "network-map-label-type";
-export const NETWORK_MAP_NODE_STRENGTH_KEY = "network-map-node-strength";
-export const NETWORK_MAP_LINK_DISTANCE_KEY = "network-map-link-distance";
-export const NETWORK_MAP_SHOW_ICONS_KEY = "network-map-show-icons";
+export const NETWORK_MAP_CONFIG_KEY = "network-map-config";
 
 //-- Multi-Instance
 export const MULTI_INSTANCE_SHOW_SOURCE_NAME_KEY = "multi-instance-show-source-name";
