7bbb93cadedbfef2d03e3b6981320c44a65d722c
Andrei LAZAROV <andrei_lazarov@yahoo.com>
2026-04-02 01:01:05 +0300

    fix: make device description not mono-space

--- src/components/device-page/tabs/DeviceInfo.tsx
+++ src/components/device-page/tabs/DeviceInfo.tsx
@@ -280,7 +280,7 @@ export default function DeviceInfo({ sourceIdx, device }: DeviceInfoProps) {
                     </span>
                 </div>
                 <div>
-                    <pre className="inline text-wrap break-all">{device.description || ""}</pre>
+                    {device.description || ""}
                     <DeviceControlUpdateDesc device={device} setDeviceDescription={setDeviceDescription} />
                 </div>
                 <div className="grid grid-cols-[max-content_1fr] gap-x-3 gap-y-2 text-wrap">
